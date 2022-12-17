a75e7c66564ddacedd5529e94cff7baaf81b6f06
BarbTheOnlyOne <79636293+BarbTheOnlyOne@users.noreply.github.com>
2022-12-17 11:04:58 +0100

    Day 6

---
+++ AdventOfCode/AdventOfCode.Day6/PacketMarker.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Day6
+{
+    internal class PacketMarker
+    {
+        private readonly int bufferSize;
+        private readonly List<string> _buffer;
+        
+        private int markerStartLocation = default;
+
+        public int StartOfpacketMarker { get; private set; }
+
+        public PacketMarker(int bufferSize)
+        {
+            this.bufferSize = bufferSize;
+            _buffer = new();
+        }
+
+        public bool AddCharacterAndCheckForStart(string character)
+        {
+            if (_buffer.Count == bufferSize)
+            {
+                _buffer.RemoveAt(0);
+            }
+
+            _buffer.Add(character);
+            markerStartLocation++;
+
+            return _buffer.Count == bufferSize ? CheckForStart() : false;
+        }
+
+        public int GetMarkerLocation()
+        {
+            return markerStartLocation;
+        }
+
+        private bool CheckForStart()
+        {
+            return _buffer.Count == _buffer.Distinct().Count();
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Day6;
+
+var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+var loadedFile = File.ReadAllText(filePath);
+
+var packetMarker = new PacketMarker(4);
+int markerLocation = default;
+
+foreach (var character in loadedFile)
+{
+    if (packetMarker.AddCharacterAndCheckForStart(char.ToString(character)))
+    {
+        markerLocation = packetMarker.GetMarkerLocation();
+        break;
+    }
+}
+
+var packetMarkerPartTwo = new PacketMarker(14);
+int messageLocation = default;
+
+foreach (var character in loadedFile)
+{
+    if (packetMarkerPartTwo.AddCharacterAndCheckForStart(char.ToString(character)))
+    {
+        messageLocation = packetMarkerPartTwo.GetMarkerLocation();
+        break;
+    }
+}
+
+Console.WriteLine("Result of the first part:");
+Console.WriteLine(markerLocation);
+Console.WriteLine("Result of the second part:");
+Console.WriteLine(messageLocation);
